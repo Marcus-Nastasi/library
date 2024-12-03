148739e6f04ada31599aa4d751bbe14f06dcaaed
Marcus-Nastasi <vinnie.nsts@gmail.com>
2024-12-03 17:42:28 -0300

    refactoring swagger config location

--- back-end/library/src/main/java/com/app/library/infrastructure/configuration/swagger/SwaggerConfig.java
+++ back-end/library/src/main/java/com/app/library/infrastructure/configuration/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.app.library.adapters.resource.swagger;
+package com.app.library.infrastructure.configuration.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
@@ -6,11 +6,9 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @ComponentScan
-@RestController
 @OpenAPIDefinition(info = @Info(title = "Library", description = "A library application back-end", version = "1.0.0"))
 @SecurityScheme(
         name = "Bearer Authentication",
