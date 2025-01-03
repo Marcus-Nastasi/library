26f87ead40afb017dbc24e76ed2706f5f722b8dc
Marcus-Nastasi <vinnie.nsts@gmail.com>
2025-01-02 22:03:17 -0300

    Implementing infra exception error handler on GlobalExceptionHandler.

--- back-end/library/src/main/java/com/app/library/adapters/exception/GlobalExceptionHandler.java
+++ back-end/library/src/main/java/com/app/library/adapters/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.app.library.adapters.exception;
 import com.app.library.application.exception.ApplicationException;
 import com.app.library.infrastructure.exception.ForbiddenException;
 import com.app.library.domain.entity.exception.DomainException;
+import com.app.library.infrastructure.exception.InfraException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,35 +17,31 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DomainException.class)
     public ResponseEntity<Object> handleDomainException(DomainException exception, WebRequest request) {
-        return ResponseEntity
-            .badRequest()
-            .body(Map.of("error", exception.getMessage()));
+        return ResponseEntity.badRequest().body(Map.of("error", exception.getMessage()));
     }
 
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<Object> handleUseCaseException(ApplicationException exception, WebRequest request) {
-        return ResponseEntity
-            .badRequest()
-            .body(Map.of("error", exception.getMessage()));
+        return ResponseEntity.badRequest().body(Map.of("error", exception.getMessage()));
     }
 
     @ExceptionHandler(ForbiddenException.class)
     public ResponseEntity<Object> handleForbiddenException(ForbiddenException exception, WebRequest request) {
-        return ResponseEntity
-            .status(HttpStatus.FORBIDDEN).build();
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Map.of("error", exception.getMessage()));
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<Object> handleInfraException(InfraException exception, WebRequest request) {
+        return ResponseEntity.badRequest().body(Map.of("error", exception.getMessage()));
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Object> handleUnknownRuntime(RuntimeException exception, WebRequest request) {
-        return ResponseEntity
-            .internalServerError()
-            .body(Map.of("error", exception.getMessage()));
+        return ResponseEntity.internalServerError().body(Map.of("error", exception.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleUnknownError(Exception exception, WebRequest request) {
-        return ResponseEntity
-            .internalServerError()
-            .body(Map.of("error", exception.getMessage()));
+        return ResponseEntity.internalServerError().body(Map.of("error", exception.getMessage()));
     }
 }
